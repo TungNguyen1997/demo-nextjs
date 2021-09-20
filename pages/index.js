f08d26c462beecedf43a7a40810415515a52c554
TungNguyen <tung.nguyen@timo.vn>
2021-09-20 14:23:51 +0700

    aa

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,15 +18,23 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing{" "}
+          <code de className={styles.code}>
+            pages/index.js
+          </code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+          {/* <a href="https://nextjs.org/docs" className={styles.card}>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </a> */}
+
+          <Link href="/about">
+            <div className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <a>About Us</a>
+            </div>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
@@ -58,12 +67,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
